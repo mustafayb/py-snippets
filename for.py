bb577ce151a0f5642cfdd645e4264439bcacd479
mustafa <my@kali.kali>
2021-04-16 17:04:44 +0200

    subproces

---
+++ for.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+for group in {'G_1SNB_D1', 'G_1SNB_D2', 'G_1SNB_D3', 'G_1SNB_D4', 'G_2SNB_A', 'U_INFNWB_A', 'G_1SNB_D2'}:
+    print(group)
---
+++ subprocess_date.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import subprocess
+time = subprocess.check_output(['date'])
+print time
